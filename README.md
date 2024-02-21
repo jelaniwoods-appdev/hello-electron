8fec26b7901f978e5ad5a526ee1d91ce486f4e19
Jelani Woods <jelani@firstdraft.com>
2024-02-20 22:15:27 -0600

    Open a window if none are open

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Project to learn electron basics.
 
 - The entry point of any Electron application is its main script. This script controls the main process, which runs in a full Node.js environment.
 - In Electron, each window displays web contents that can be loaded from either a local HTML file or a remote URL.
+- You can use the process global's `platform` attribute to run code specifically for certain operating systems.
--- main.js
+++ main.js
@@ -11,4 +11,15 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+  // Open a window if none are open (macOS)
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
 })
+
+// Quit the app when all windows are closed (Windows & Linux)
+app.on("window-all-closed", () => {
+  // darwin, meaning macOS
+  if (process.platform !== "darwin") app.quit()
+})
+
